3bef39d0d6ac66c7a91d3072b3ccdd210d7f858d
robertocml <roberto.perezb@outlook.com>
2020-02-04 00:05:05 -0600

    lib fix

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pytube as pt
 
+# I made the fix of bennyb518.. on next link to make mixins.py inside the pytube library works..
+# https://github.com/nficano/pytube/issues/467#issuecomment-567560796
 
 
+# Basic example to start with
 video_url = 'https://www.youtube.com/watch?v=jNQXAC9IVRw'
-video =  pt.YouTube(video_url)
-video = video.streams.first()
-video.download('C:\Users\\rober\Desktop\Github Repos\Pyphant')
+youtube =  pt.YouTube(video_url)
+video = youtube.streams.first()
+video.download()
