39cbbff2b4f0cd0a6444c340e2b244fc1e0577d1
robertocml <roberto.perezb@outlook.com>
2020-02-05 23:44:46 -0600

    sending the file from directory is now working

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import pytube as pt
-from flask import Flask, escape, request, render_template,url_for
+from flask import Flask, escape, request, render_template,url_for, send_from_directory
 
 
 # I made the fix of bennyb518.. on next link to make mixins.py inside the pytube library works..
 # https://github.com/nficano/pytube/issues/467#issuecomment-567560796
-
+# https://www.youtube.com/watch?v=jNQXAC9IVRw
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,8 +17,10 @@ def video_url_post():
     try:
         youtube = pt.YouTube(video_url)
         video = youtube.streams.first()
-        video.downlod('C:\\Users\\rober') 
-        return render_template('index.html')
+        video.download('C:\\Users\\rober\\Desktop\\Github Repos\\Pyphant\\uploads') 
+        filename = video.title + ".mp4"
+        print(filename)
+        return send_from_directory(directory='uploads', filename=filename, as_attachment=True)
     except:
         print('failed')
         return render_template('index.html')
