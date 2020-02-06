022326d1e4fa3711ac594938876e0c7336302aa9
robertocml <roberto.perezb@outlook.com>
2020-02-05 23:22:27 -0600

    needs a path...

--- app.py
+++ app.py
@@ -5,13 +5,6 @@ from flask import Flask, escape, request, render_template,url_for
 # I made the fix of bennyb518.. on next link to make mixins.py inside the pytube library works..
 # https://github.com/nficano/pytube/issues/467#issuecomment-567560796
 
-
-# Basic example to start with
-# video_url = 'https://www.youtube.com/watch?v=jNQXAC9IVRw'
-# youtube =  pt.YouTube(video_url)
-# video = youtube.streams.first()
-# video.download()
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,10 +14,13 @@ def index():
 @app.route('/',methods=['GET','POST'])
 def video_url_post():
     video_url = request.form['video_url']
-    youtube = pt.YouTube(video_url)
-    video = youtube.streams.first()
-    video.download() 
-    return render_template('index.html')
+    try:
+        youtube = pt.YouTube(video_url)
+        video = youtube.streams.first()
+        video.download() 
+        return render_template('sucess.html')
+    except:
+        return render_template('failed.html')
 
 
 if __name__ == "__main__":
